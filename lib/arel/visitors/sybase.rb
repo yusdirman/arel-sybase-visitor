970694a6ac10da7b5fd71c64a9c451ef6f4a50ba
Marcello Barnaba <vjt@openssl.it>
2011-02-25 11:11:39 +0100

    Replace the empty string with NULL on INSERT and UPDATE - see the source for details

--- lib/arel/visitors/sybase.rb
+++ lib/arel/visitors/sybase.rb
@@ -1,16 +1,21 @@
-# Arel Sybase ASE 15.0 Visitor
+# Arel Sybase ASE 15.0 Visitor -- implementing
+# LIMIT and OFFSET using cursors and replacing
+# the empty string with NULL in the INSERT and
+# UPDATE statements.
 #
 # Authors:
 #
 # Mirek Rusin      <mirek@me.com>
 # Marcello Barnaba <vjt@openssl.it>
 #
+# Released under the terms of the Ruby License
+#
 module Arel
   module Visitors
     class Sybase < Arel::Visitors::ToSql
       private
 
-        def visit_Arel_Nodes_SelectStatement o
+        def visit_Arel_Nodes_SelectStatement(o)
           limit, offset = o.limit.try(:expr) || 0, o.offset.try(:expr) || 0
 
           # Alter the node if a limit or offset are set
@@ -48,6 +53,55 @@ module Arel
             CLOSE #{cursor}
             DEALLOCATE #{cursor}
           eosql
+        end
+
+        # On ASE 15.0 the empty string has a puzzling behaviour:
+        #
+        # http://infocenter.sybase.com/help/index.jsp?topic=/com.sybase.help.ase_15.0.blocks/html/blocks/blocks262.htm
+        #
+        # Meaning that "" becomes " " on INSERT, and assigments:
+        #
+        # 1> select '"' || '' || '"'
+        # 2> go
+        #
+        # ---
+        #   " "
+        #
+        # Because ' ' is NOT an empty string, and it creeps up
+        # till to the ERB views, it requires that WHERE clauses
+        # take care of it, etc - we replace it with a NULL here
+        #
+        # OK, '' is different than a NULL, but it seems that on
+        # Sybase this is not the case. For instance:
+        #
+        # 1> select ltrim('')
+        # 2> go
+        #
+        # -
+        #   NULL
+        #
+        # Anyway, tables created with Rails migrations set columns
+        # nullable by default so this should not be an issue as
+        # long as you don't update legacy tables used by legacy
+        # software that actually expects " " to represent the empty
+        # string ...
+        def visit_Arel_Nodes_UpdateStatement(o)
+          o.values.each do |assignment|
+            assignment.right = null_if_empty_string(assignment.right)
+          end
+          super
+        end
+
+        # See +visit_Arel_Nodes_UpdateStatement+ documentation
+        def visit_Arel_Nodes_InsertStatement(o)
+          o.values.expressions.map! &method(:null_if_empty_string)
+          super
+        end
+
+        # Returns value unless it is a string and it is empty,
+        # in that case nil is returned.
+        def null_if_empty_string(value)
+          value unless value.is_a?(String) && value.length.zero?
         end
 
     end
